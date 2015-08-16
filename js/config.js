c637be8f5fc26438f1fa5e3f20a4944d3870b4f9
EpicXGamer <zachary.colone@yahoo.com>
2015-08-16 14:14:34 -0400

    refreshed

--- js/config.js
+++ js/config.js
@@ -1,23 +1,7 @@
-/*
---------------------------------------
---------LEAKED BY ANONYMOUS LEAKR --------------
-----------------------------------------
-      _           _    _                     _ 
-     | |         | |  | |                   | |
-  ___| | ___  ___| | _| |     ___   __ _  __| |
- / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
- \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
- |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
-    version 2.04
-	
---------------------------------------
---------LEAKED BY ANONYMOUS LEAKR --------------
-----------------------------------------	
-*/
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "images/logo.png";
+var l_serverImage = "logo.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
@@ -25,7 +9,7 @@ var l_serverName = "ZX Zone";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
-var l_bgVideo = false;
+var l_bgVideo = true;
 
 /*Video background
 Avaiable options:
@@ -52,7 +36,7 @@ Avaiable options:
 
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
-var l_background = "backgrounds/videos/Fondos_animados_fantasia_mix_Full_HD_animated_back.webm";
+var l_background = "4K_Computer_Tech_Loading_Data_Animation_Background.webm";
 
 /*(Image-background only)
 Place one or more images
@@ -93,7 +77,7 @@ var l_musicDisplay = true;
 /*Music playlist
 Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
-	{youtube: "BviQFUIln8E", name: "BF4"}
+	{youtube: "iUCLLbK0ijU", name: "Take Back New York"}
 ];
 
 /*Random music order?*/
@@ -101,7 +85,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 35;
+var l_musicVolume = 45;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
@@ -111,6 +95,9 @@ var l_messages = [
 "Enjoy the server",
 "Join our Steam Group ZX ZONE for upcomming events like deathrun!",
 "Swearing is definently not allowed so dont think about it"
+"Don't spawnkill... thats very important.. if they pull out a weapon then you can... this doesn't include toolguns or physguns"
+"EpicGamer241 is the owner... if someone says that their the owner then report to the administrators or me"
+"wan't to know more rules? type !motd in the chat"
 ];
 
 /*Random message order?*/
@@ -123,9 +110,3 @@ var l_messagesDelay = 5000;
 /*Messages fade time
 in milliseconds*/
 var l_messagesFade = 1000;
-
-/*
---------------------------------------
---------LEAKED BY ANONYMOUS LEAKR ----https://www.youtube.com/watch?v=BviQFUIln8E----------
-----------------------------------------
-*/
